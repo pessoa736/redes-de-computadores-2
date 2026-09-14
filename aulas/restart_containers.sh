2a8762056e772c208a565e9439f390a8d4255759
Davi Passos <pessoa736@github.io>
2026-09-14 11:13:48 -0300

    chore: ao execultar o restart_containers.sh, agora mostra os containers e imagens ativas

--- aulas/restart_containers.sh
+++ aulas/restart_containers.sh
@@ -2,3 +2,10 @@
 docker compose down -v
 docker compose up -d --build --remove-orphans --force-recreate
 
+
+echo
+echo
+echo "abra: http://localhost:8080"
+
+
+sleep 5 && ./listar-imagems-e-container.sh
